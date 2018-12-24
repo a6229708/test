ef863d47e11d7e6870b3ae6b638aecf768670e77
a6229708 <34296830+a6229708@users.noreply.github.com>
2018-12-24 11:40:49 +0800

    11

---
+++ src/Entity/Users.java
@@ -0,0 +1,104 @@
+package Entity;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Users {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String username;
+    private String password;
+    private String isadmin;
+    private Collection<House> houses;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 20)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "phone", nullable = true, length = 11)
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Basic
+    @Column(name = "username", nullable = false, length = 20)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 20)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "isadmin", nullable = false, length = 20)
+    public String getIsadmin() {
+        return isadmin;
+    }
+
+    public void setIsadmin(String isadmin) {
+        this.isadmin = isadmin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return Objects.equals(id, users.id) &&
+                Objects.equals(name, users.name) &&
+                Objects.equals(phone, users.phone) &&
+                Objects.equals(username, users.username) &&
+                Objects.equals(password, users.password) &&
+                Objects.equals(isadmin, users.isadmin);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, phone, username, password, isadmin);
+    }
+
+    @OneToMany(mappedBy = "users")
+    public Collection<House> getHouses() {
+        return houses;
+    }
+
+    public void setHouses(Collection<House> houses) {
+        this.houses = houses;
+    }
+}
